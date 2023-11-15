7ded28d696a13df2320740007c2e6ee72b9b6af6
1Ram707 <126853321+1Ram707@users.noreply.github.com>
2023-11-15 22:45:02 +0000

    Updated Student
    Updated Student (Query and Access learning Web view

--- app/src/main/java/com/example/myvc/authorization/student/fragments/sQueryFragment.kt
+++ app/src/main/java/com/example/myvc/authorization/student/fragments/sQueryFragment.kt
@@ -5,56 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.example.myvc.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [sQueryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class sQueryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var webViewQuery: WebView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_s_query, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_s_query, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment sQueryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            sQueryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // Initialize WebView
+        webViewQuery = view.findViewById(R.id.webViewQuery)
+        webViewQuery.settings.javaScriptEnabled = true // Enable JavaScript if needed
+        webViewQuery.webViewClient = WebViewClient()
+
+        // Load the URL
+        webViewQuery.loadUrl("https://portal.varsitycollege.co.za/assist/student-hub/")
+
+        return view
     }
 }
