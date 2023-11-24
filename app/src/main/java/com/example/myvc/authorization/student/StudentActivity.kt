57cbbe629d05e832e8537b7e26c00155b5b90556
Theo399 <ST10091167@vcconnect.edu.za>
2023-11-24 14:17:09 +0000

    MyVC

--- app/src/main/java/com/example/myvc/authorization/student/StudentActivity.kt
+++ app/src/main/java/com/example/myvc/authorization/student/StudentActivity.kt
@@ -2,6 +2,7 @@
 
 package com.example.myvc.authorization.student
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -9,9 +10,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.myvc.R
+import com.example.myvc.authorization.student.fragments.sAssessmentFragment
+import com.example.myvc.authorization.student.fragments.sAssignmentFragment
+import com.example.myvc.authorization.student.fragments.sAvailabilityFragment
 import com.example.myvc.authorization.student.fragments.sCommunicateFragment
 import com.example.myvc.authorization.student.fragments.sHomeFragment
 import com.example.myvc.authorization.student.fragments.sLogoutFragment
+import com.example.myvc.authorization.student.fragments.sMarkFragment
+import com.example.myvc.authorization.student.fragments.sMaterialFragment
+import com.example.myvc.authorization.student.fragments.sQueryFragment
+import com.example.myvc.authorization.student.fragments.sScheduleFragment
 import com.example.myvc.databinding.ActivityStudentBinding
 import com.google.android.material.navigation.NavigationView
 
@@ -49,6 +57,20 @@ class StudentActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
                 .replace(R.id.frameLayout, sHomeFragment()).commit()
             R.id.nav_s_communicate -> supportFragmentManager.beginTransaction()
                 .replace(R.id.frameLayout, sCommunicateFragment()).commit()
+            R.id.nav_s_material -> supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, sMaterialFragment()).commit()
+            R.id.nav_s_query -> supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, sQueryFragment()).commit()
+            R.id.nav_s_assignment -> supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, sAssignmentFragment()).commit()
+            R.id.nav_s_mark -> supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, sMarkFragment()).commit()
+            R.id.nav_s_availability -> supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, sAvailabilityFragment()).commit()
+            R.id.nav_s_assessment -> supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, sAssessmentFragment()).commit()
+            R.id.nav_s_schedule -> supportFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, sScheduleFragment()).commit()
             R.id.nav_s_logout -> supportFragmentManager.beginTransaction()
                 .replace(R.id.frameLayout, sLogoutFragment()).commit()
         }
