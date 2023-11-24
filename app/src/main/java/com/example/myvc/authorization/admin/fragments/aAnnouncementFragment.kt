535204bc395007e3b929136062c718ddddb2f67d
Theo399 <ST10091167@vcconnect.edu.za>
2023-11-24 15:21:29 +0000

    MyVC

--- app/src/main/java/com/example/myvc/authorization/admin/fragments/aAnnouncementFragment.kt
+++ app/src/main/java/com/example/myvc/authorization/admin/fragments/aAnnouncementFragment.kt
@@ -1,10 +1,16 @@
 package com.example.myvc.authorization.admin.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.example.myvc.R
 
 class aAnnouncementFragment : Fragment() {
@@ -15,5 +21,47 @@ class aAnnouncementFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_a_announcement, container, false)
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val myWebView: WebView = view.findViewById(R.id.webView)
+        myWebView.webViewClient = object : WebViewClient() {
+            @Deprecated("Deprecated in Java")
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                url: String
+            ): Boolean {
+                view.loadUrl(url)
+                return true
+
+
+            }
+
+        }
+        val webSetting: WebSettings = myWebView.settings
+        webSetting.javaScriptEnabled = true
+        myWebView.webViewClient = WebViewClient()
+
+        myWebView.canGoBack()
+        myWebView.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK
+
+                && event.action == MotionEvent.ACTION_UP
+                && myWebView.canGoBack()){
+                myWebView.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
+
+
+        myWebView.loadUrl("https://classroom.google.com/h")
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.allowContentAccess = true
+        myWebView.settings.domStorageEnabled = true
+        myWebView.settings.useWideViewPort = true
+
+
     }
 }
